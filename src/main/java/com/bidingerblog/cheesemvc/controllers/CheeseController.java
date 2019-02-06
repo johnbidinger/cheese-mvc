4c2ca7d9d472ecac65c9eaff0ede5e08d507f2c5
Joe Bidinger <johnbidinger@gmail.com>
2019-02-06 16:17:11 -0500

    Added styling for table

--- src/main/java/com/bidingerblog/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/bidingerblog/cheesemvc/controllers/CheeseController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+//    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String , String > cheeses = new HashMap<>();
 
     //Request path: /cheese
     @RequestMapping(value = "")
@@ -30,8 +32,8 @@ public class CheeseController {
         return "cheese/add";
     }
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName){
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDesc){
+        cheeses.put(cheeseName, cheeseDesc);
         //Redirect to /cheese
         return "redirect:";
     }
